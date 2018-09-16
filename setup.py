948d576a19b08d8f1e4f2b39335c58a3b8e489b9
Chris <christopher.kluger@gmx.de>
2018-09-16 22:12:14 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+
+with open('README.rst') as fp:
+    long_description = fp.read()
+
+CLASSIFIERS = """
+Development Status :: 3 - Alpha
+Intended Audience :: Science/Research
+Intended Audience :: Developers
+Programming Language :: Python :: 3.6
+Topic :: Software Development
+Topic :: Scientific/Engineering
+"""
+
+setup(
+    name='stockdatareader',
+    version='0.1.0',
+    author='',
+    author_email='euphiment@gmx.de',
+    url='',
+    description='Read online stock data',
+    long_description=long_description,
+    packages=find_packages(),
+    classifiers=[f for f in CLASSIFIERS.split('\n') if f],
+    install_requires=['pandas',
+                      'mechanicalsoup',
+                      'stdnum',
+                      'mechanicalsoup'],
+)
